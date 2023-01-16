a0ea01ea5d06a2ad2c850721bb7f4909d3ee45f7
Sanhil143 <sanhilrai143@gmail.com>
2023-01-16 14:22:21 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const router = require('./routers/router')
 mongoose.set('strictQuery', true)
 
+dotenv.config()
 const app = express()
 app.use(express.json())
 
 
-mongoose.connect("mongodb+srv://sanhil143:raisahab12345@sanhildb.kk3knyj.mongodb.net/BlockChain")
+mongoose.connect(process.env.MY_DB_URL)
       .then(() => console.log("My DB is connected"))
       .catch((err) => console.error(err))
 
 app.use('/', router)
 
-app.listen(3000, () => {
-      console.log("Express app running on port " + 3000);
+app.listen(process.env.PORT || 4000, () => {
+      console.log("Express app running on port " + process.env.PORT || 4000);
 })
 
+ 
